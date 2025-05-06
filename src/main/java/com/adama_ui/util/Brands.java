671ebd4211ad620e914e1c735f91947d707b649e
Mazer-R <manel.cerezo1@gmail.com>
2025-05-06 20:13:34 +0200

    Added Brands Enum

---
+++ src/main/java/com/adama_ui/util/Brands.java
@@ -0,0 +1,27 @@
+package com.adama_ui.util;
+
+public enum Brands {
+    Samsung,
+    Apple,
+    Logitech,
+    Nvidia,
+    Asus,
+    HP,
+    Lenovo,
+    MSI,
+    Acer,
+    Intel,
+    AMD,
+    Seagate,
+    WesternDigital,
+    Crucial,
+    Kingston,
+    Corsair,
+    Razer,
+    CoolerMaster,
+    Adobe,
+    Microsoft,
+    OpenAI,
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,6 @@ module com.adama_ui {
 
     opens com.adama_ui to javafx.fxml;
     exports com.adama_ui;
+    exports com.adama_ui.util;
+    opens com.adama_ui.util to javafx.fxml;
 }
